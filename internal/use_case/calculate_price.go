fdad380ce7e116a68b0235dd58a9802c4ca62b30
moreirak14 <kaiquemoreiracloud@gmail.com>
2023-09-13 23:42:33 -0300

    feature: create calculate final price use case

---
+++ internal/use_case/calculate_price.go
@@ -0,0 +1,51 @@
+package use_case
+
+import "github.com/moreirak14/taxes-api/internal/entity"
+
+type OrderInput struct {
+	ID    string
+	Price float64
+	Tax   float64
+}
+
+type OrderOutput struct {
+	ID         string
+	Price      float64
+	Tax        float64
+	FinalPrice float64
+}
+
+type CalculateFinalPriceUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+}
+
+func NewCalculateFinalPriceUseCase(orderRepository entity.OrderRepositoryInterface) *CalculateFinalPriceUseCase {
+	return &CalculateFinalPriceUseCase{
+		OrderRepository: orderRepository,
+	}
+}
+
+func (c *CalculateFinalPriceUseCase) Execute(input OrderInput) (*OrderOutput, error) {
+	order, err := entity.NewOrder(input.ID, input.Price, input.Tax)
+	if err != nil {
+		return nil, err
+	}
+
+	err = order.CalculateFinalPrice()
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.OrderRepository.Save(order)
+	if err != nil {
+		return nil, err
+	}
+
+	return &OrderOutput{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.FinalPrice,
+	}, nil
+
+}
