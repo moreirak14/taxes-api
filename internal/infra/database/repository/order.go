4dde6a20f965f2475594911e3e434519c512d37c
moreirak14 <kaiquemoreiracloud@gmail.com>
2023-09-13 23:26:38 -0300

    feature: create order repository with db queries (save and get total)

---
+++ internal/infra/database/repository/order.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"database/sql"
+
+	"github.com/moreirak14/taxes-api/internal/entity"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	_, err := r.Db.Exec("INSERT INTO orders (id, price, tax, final_price) VALUES ($1, $2, $3, $4)", order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *OrderRepository) GetTotalTransactions() (int, error) {
+	var total int
+	err := r.Db.QueryRow("SELECT COUNT(*) FROM orders").Scan(&total)
+	if err != nil {
+		return 0, err
+	}
+
+	return total, nil
+}
